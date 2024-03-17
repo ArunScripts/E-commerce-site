6d05a491fb440c7ad740b3648e4051a49deaec48
arun464646 <arunkumarjayaraman464@gmail.com>
2024-03-17 16:21:39 +0530

    feat: 60% of work is completed

--- amzclone.js
+++ amzclone.js
@@ -1,1 +1,43 @@
-console.log(" this is arunnn");
+const imgs= document.querySelectorAll('.header-slider ul img');
+const prev_btn= document.querySelector('.control-preview');
+const nxt_btn= document.querySelector('.control-next');
+
+let n=3;
+
+function changeslider(){ 
+    for (let i = 0; i<imgs.length; i++){
+       imgs[i].style.display='none';
+    }
+    imgs[n].style.display='block'
+}
+changeslider()
+
+prev_btn.addEventListener('click',(e)=>{
+    if(n>0){
+        n--;
+    }
+    else{
+        n=imgs.length-1;
+    }
+    changeslider()
+})
+
+nxt_btn.addEventListener('click',(e)=>{
+    if(n<imgs.length-1){
+        n++;
+    }
+    else{
+        n=0;
+    }
+    changeslider()
+
+})
+
+const scroolcontainer=document.querySelectorAll('.products');
+
+for(const item of scroolcontainer){
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft +=evt.deltaY;
+    })
+}
